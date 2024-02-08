651a391440c79d288c0045d1b56c991b874b04d8
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2024-02-08 12:32:55 -0500

    Add Nvim

---
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,69 @@
+return {
+  {
+
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("refactoring").setup()
+    end,
+  },
+  {
+    "junegunn/vim-easy-align",
+  },
+  {
+    "L3MON4D3/LuaSnip", -- snippets
+    event = "InsertEnter",
+    dependencies = {
+      "psto/friendly-snippets",
+      config = function()
+        -- specify the path so that friendly-snippets are not duplicated
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
+        require("luasnip").filetype_extend("typescript", { "javascript" })
+        require("luasnip").filetype_extend("typescriptreact", { "javascriptreact" })
+      end,
+    },
+  },
+
+  {
+    "chrisgrieser/nvim-scissors",
+    dependencies = {
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      jsonFormatter = "jq",
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>cS", function() require("scissors").editSnippet() end, desc = "Edit Snippets" },
+      { "<leader>cs", function() require("scissors").addNewSnippet() end, desc = "Add Snippets" },
+    },
+  },
+
+  -- Incremental rename
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = true,
+  },
+
+  -- better diffing
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    opts = {},
+    keys = { { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" } },
+  },
+
+  -- fugitive: Git blame and open in GitHub
+  {
+    "tpope/vim-fugitive",
+    lazy = true,
+    cmd = "Git",
+    keys = {
+      { "<leader>gb", "<cmd>Git blame<cr>", desc = "Git Blame" },
+    },
+  },
+}
